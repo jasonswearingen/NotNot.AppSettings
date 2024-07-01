88d077d990ead7346c8d8f45d6eb422700063556
JasonS <jasons@novaleaf.com>
2024-07-01 15:40:34 -0700

    add ability to load direct from a string containing your json

--- NotNot.AppSettings/AppSettingsGen.cs
+++ NotNot.AppSettings/AppSettingsGen.cs
@@ -202,61 +202,97 @@ public partial class AppSettings
 /// </summary>
 public partial class AppSettingsBinder : IAppSettingsBinder
 {{
-   public AppSettings AppSettings {{ get; protected set; }}
+	public AppSettings AppSettings {{ get; protected set; }}
 
-   public AppSettingsBinder(IConfiguration _config)
-   {{
-      AppSettings = new AppSettings();
+	public AppSettingsBinder(IConfiguration _config)
+	{{
+		AppSettings = new AppSettings();
 
-      //automatically reads and binds to config file
-      _config.Bind(AppSettings);
-   }}
+		//automatically reads and binds to config file
+		_config.Bind(AppSettings);
+	}}
 
-   /// <summary>
-   /// Manually construct an AppSettings from your appsettings.json files.
-   /// <para>NOTE: This method is provided for non-DI users.  If you use DI, don't use this method.  Instead just register this class as a service.</para>
-   /// </summary>
-   /// <param name=""appSettingsLocation"">folder where to search for appsettings.json.  defaults to current app folder.</param>
-   /// <param name=""appSettingsFileNames"">lets you override the files to load up.  defaults to 'appsettings.json' and 'appsettings.{{DOTNET_ENVIRONMENT}}.json'</param>
-   /// <param name=""throwIfFilesMissing"">default is to silently ignore if any of the .json files are missing.</param>
-   /// <returns>your strongly typed appsettings with values from your .json loaded in</returns>
-   public static AppSettings LoadDirect(string? appSettingsLocation = null,IEnumerable<string>? appSettingsFileNames=null,bool throwIfFilesMissing=false )
-   {{      
-      //pick what .json files to load
-      if (appSettingsFileNames is null)
-      {{
-         //figure out what env
-         var env = Environment.GetEnvironmentVariable(""ASPNETCORE_ENVIRONMENT"");
-         env ??= Environment.GetEnvironmentVariable(""DOTNET_ENVIRONMENT"");
-         env ??= Environment.GetEnvironmentVariable(""ENVIRONMENT"");
-         //env ??= ""Development""; //default to ""Development
-         if (env is null)
-         {{
-            appSettingsFileNames = new[] {{ ""appsettings.json"" }};
-         }}
-         else
-         {{
-            appSettingsFileNames = new[] {{ ""appsettings.json"", $""appsettings.{{env}}.json"" }};
-         }}
-      }}
+	/// <summary>
+	/// Manually construct an AppSettings from your appsettings.json files.
+	/// <para>NOTE: This method is provided for non-DI users.  If you use DI, don't use this method.  Instead just register this class as a service.</para>
+	/// </summary>
+	/// <param name=""appSettingsLocation"">folder where to search for appsettings.json.  defaults to current app folder.</param>
+	/// <param name=""appSettingsFileNames"">lets you override the files to load up.  defaults to 'appsettings.json' and 'appsettings.{{DOTNET_ENVIRONMENT}}.json'</param>
+	/// <param name=""throwIfFilesMissing"">default is to silently ignore if any of the .json files are missing.</param>
+	/// <returns>your strongly typed appsettings with values from your .json loaded in</returns>
+	public static AppSettings LoadDirect(string? appSettingsLocation = null,IEnumerable<string>? appSettingsFileNames=null,bool throwIfFilesMissing=false )
+	{{      
+		//pick what .json files to load
+		if (appSettingsFileNames is null)
+		{{
+			//figure out what env
+			var env = Environment.GetEnvironmentVariable(""ASPNETCORE_ENVIRONMENT"");
+			env ??= Environment.GetEnvironmentVariable(""DOTNET_ENVIRONMENT"");
+			env ??= Environment.GetEnvironmentVariable(""ENVIRONMENT"");
+			//env ??= ""Development""; //default to ""Development
+			if (env is null)
+			{{
+				appSettingsFileNames = new[] {{ ""appsettings.json"" }};
+			}}
+			else
+			{{
+				appSettingsFileNames = new[] {{ ""appsettings.json"", $""appsettings.{{env}}.json"" }};
+			}}
+		}}
 
-      //build a config from the specified files
-      var builder = new ConfigurationBuilder();
-      if (appSettingsLocation != null)
-      {{
-         builder.SetBasePath(appSettingsLocation);
-      }}
-      var optional = !throwIfFilesMissing;
-      foreach (var fileName in appSettingsFileNames)
-      {{         
-         builder.AddJsonFile(fileName, optional: optional, reloadOnChange: false); // Add appsettings.json
-      }}
-      IConfigurationRoot configuration = builder.Build();
+		//build a config from the specified files
+		var builder = new ConfigurationBuilder();
+		if (appSettingsLocation != null)
+		{{
+			builder.SetBasePath(appSettingsLocation);
+		}}
+		var optional = !throwIfFilesMissing;
+		foreach (var fileName in appSettingsFileNames)
+		{{         
+			builder.AddJsonFile(fileName, optional: optional, reloadOnChange: false); // Add appsettings.json
+		}}
+		IConfigurationRoot configuration = builder.Build();
 
-      //now finally get the appsettings we care about
-      var binder = new AppSettingsBinder(configuration);
-      return binder.AppSettings;
-   }}
+		//now finally get the appsettings we care about
+		var binder = new AppSettingsBinder(configuration);
+		return binder.AppSettings;
+	}}
+
+	/// <summary>
+	/// helper to create an AppSettings from a string containing your json
+	/// </summary>
+	/// <param name=""appSettingsJsonText""></param>
+	/// <returns></returns>
+	public static AppSettings LoadDirectFromText(string appSettingsJsonText)
+	{{
+	
+
+		//build a config from the specified files
+		var builder = new ConfigurationBuilder();
+
+		var configurationBuilder = new ConfigurationBuilder();
+
+		IConfigurationRoot configuration;
+		using (var stream = new MemoryStream())
+		{{
+			using (var writer = new StreamWriter(stream))
+			{{
+				writer.Write(appSettingsJsonText);
+				writer.Flush();
+				stream.Position = 0;
+				configurationBuilder.AddJsonStream(stream);
+
+
+
+				configuration = configurationBuilder.Build();
+			}}
+		}}
+
+
+		//now finally get the appsettings we care about
+		var binder = new AppSettingsBinder(configuration);
+		return binder.AppSettings;
+	}}
 }}
 
 /// <summary>
@@ -270,7 +306,7 @@ public partial class AppSettingsBinder : IAppSettingsBinder
 /// </summary>
 public interface IAppSettingsBinder
 {{
-   public AppSettings AppSettings {{ get; }}
+	public AppSettings AppSettings {{ get; }}
 }}
 ");
 
